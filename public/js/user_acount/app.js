ae391c48cd7dac16f33f7417efda02ab57c303b1
poriyaASadi <133978734+poriyaASadi@users.noreply.github.com>
2023-12-11 13:24:35 +0330

    add code user account

--- public/js/user_acount/app.js
+++ public/js/user_acount/app.js
@@ -7,7 +7,9 @@ const form_show_dialog = $.getElementById('form_show_dialog')
 
 form_open_dialog.addEventListener('click' , () => {
   dialog.showModal();
+  $.getElementById('show_slid').classList.add('h-full')
 })
  form_show_dialog.addEventListener('click' , () => {
    dialog.close()
+   $.getElementById('show_slid').classList.remove('h-full')
  })
