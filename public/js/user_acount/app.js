7d27d3e8b990dbe23228a28ac00303f881294ca2
poriyaASadi <133978734+poriyaASadi@users.noreply.github.com>
2023-12-14 17:04:40 +0330

    log projeact

--- public/js/user_acount/app.js
+++ public/js/user_acount/app.js
@@ -3,7 +3,12 @@ let $ = document;
 const form_open_dialog = $.getElementById('form_open_dialog');
 const dialog = $.querySelector('dialog');
 const form_show_dialog = $.getElementById('form_show_dialog')
+// ----------------
+let active_icon_branch = document.getElementById('active_icon_branch');
+let stack = document.querySelectorAll('#stack_icon');
 
+
+// ---------- open dialog acocount user 
 
 form_open_dialog.addEventListener('click' , () => {
   dialog.showModal();
@@ -13,3 +18,21 @@ form_open_dialog.addEventListener('click' , () => {
    dialog.close()
    $.getElementById('show_slid').classList.remove('h-full')
  })
+// ---------- end
+ 
+// .........................
+
+//  --------- cheng active icon show post user 
+// ------------->
+stack.forEach((item) => {
+   item.addEventListener('click' , (e) => {
+    let dataValue = e.target.dataset.id
+     if (dataValue == 'user') {
+      active_icon_branch.classList.add('left-48');
+     }else {
+        active_icon_branch.classList.remove('left-48');
+     }
+   })
+})
+// --------------> end 
+
